abacccd123d05bdc38f7d5438602cfa0a65d73b8
David Curran <david.curran@gmail.com>
2015-05-11 13:07:53 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -80,13 +80,13 @@ html_doc = response.text.encode('utf-8').strip()
 
 ##print html_doc
 soup = BeautifulSoup(html_doc)
-print soup.title.string.rstrip()
-##print soup.title.string.rstrip()+ "\t"
+##print soup.title.string.rstrip()
+print soup.title.string.rstrip()+ "\t"
 spans = soup.find_all('span', {'class' : 'price'})
 lines = [span.get_text() for span in spans]
 for line in lines:
-   ##print(line).rstrip()+ "\t"
-    print(line)
+   print(line).rstrip()+ "\t"
+   ##print(line)
 
 print "\n"
 ##print response.headers
