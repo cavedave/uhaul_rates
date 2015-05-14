9cb5e7feefe28024ee44bbcdb6366f189c8e1408
David Curran <david.curran@gmail.com>
2015-05-14 11:49:56 +0100

    Update permuteCities.py

--- permuteCities.py
+++ permuteCities.py
@@ -1,1 +1,7 @@
+from itertools import *
+#program to get choose of all the cities for Uhaul to check between
+
+
+
+print (list( permutations(["New York, NY","Los Angeles, CA","Chicago, IL","Houston, TX","Philadelphia, PA", "Phoenix, AZ", "San Antonio, TX", "San Diego, CA","Dallas, TX","San Jose, CA","Austin, TX","Indianapolis, IN","Jacksonville, FL","San Francisco, CA","Columbus, OH","Charlotte, NC","Fort Worth, TX","Detroit, MI","El Paso, TX","Memphis, TN","Seattle, WA","Denver, CO","Washington, DC","Boston, MA","Nashville, TN","Baltimore, MD","Oklahoma City, OK","Louisville, KY","Portland, OR","Las Vegas, NV","Milwaukee, WI","Albuquerque, NM","Tucson, AZ","Fresno, CA"], 2)))
 
